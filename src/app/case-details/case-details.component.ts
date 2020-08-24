b9df8a79bb0ad56dfc3c09a70f1470f068c50638
nimmi <nimmy.n.k@gmail.com>
2020-08-24 10:11:36 +0530

    Latest changes

--- src/app/case-details/case-details.component.ts
+++ src/app/case-details/case-details.component.ts
@@ -13,6 +13,11 @@ export class CaseDetailsComponent implements OnInit {
   selectedItem: any;
   toggle: boolean = false;
   clinSymptoms: any;
+  disease2Symptoms: any;
+  disease2ClineExams: any;
+  diseases: any;
+  updatedItems: any;
+  ceItemList: any;
 
   constructor(private appService: AppService) { }
 
@@ -20,11 +25,26 @@ export class CaseDetailsComponent implements OnInit {
 
     this.cliexams = this.appService.getClineExams();
     this.clinSymptoms = this.appService.getClineSymptoms();
+    this.disease2ClineExams = this.appService.getDisease2ClinExams();
+    this.disease2Symptoms = this.appService.getDiseaseSymptoms();
+    this.diseases = this.appService.getDiseaseList();
   
     
   }
 
-  onSelect(item){
+  onSelect(item, flag){
+    let diseaseList = this.diseases;
+if(flag == 'ce'){
+  this.updatedItems= this.disease2ClineExams.filter((elem) => elem.ceid === item.id);
+  let list1 = this.updatedItems;
+  this.ceItemList = diseaseList.filter((elem) => list1.
+  find((elem1) => elem.id === elem1.did));
+}else{
+  this.updatedItems= this.disease2Symptoms.filter((elem) => elem.syid === item.id);
+  let list1 = this.updatedItems;
+  this.ceItemList = diseaseList.filter((elem) => list1.
+  find((elem1) => elem.id === elem1.did));
+}
     this.selectedItem = item.name;
   }
 
